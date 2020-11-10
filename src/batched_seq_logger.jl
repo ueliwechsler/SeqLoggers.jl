f795c50ee985387e332749f7ddc8494e480e3beb
Ueli Wechsler <ueli.wechsler@axpo.com>
2020-11-10 14:55:31 +0100

    improve batchedlogger with lcok and unlock to mack it thread-safe

--- src/batched_seq_logger.jl
+++ src/batched_seq_logger.jl
@@ -1,43 +1,26 @@
-# NOTE: This is not that useful at the moment!
+# NOTE: BatchSeqLogger only works correctly with `with_logger`
+# NOTE: Not yet parallelizable (we need to lock the array during posting!)
 
-# TODO: Add seqlogger with autoflush (can we do it, with automatice auto_flush_timeout?)
-
-# seqlog.log_to_seq(
-#    server_url="http://my-seq-server:5341/",
-#    api_key="My API Key",
-#    level=logging.INFO,
-#    batch_size=10,
-#    auto_flush_timeout=10,  # seconds
-#    override_root_logger=True,
-#    json_encoder_class=json.encoder.JSONEncoder  # Optional; only specify this if you want to use a custom JSON encoder
-# )
-# Batching and auto-flush
-# By default SeqLog will wait until it has a batch of 10 messages before sending them to Seq. You can control the batch size by passing a value for batch_size.
-#
-# If you also want it to publish the current batch of events when not enough of them have arrived within a certain period, you can pass auto_flush_timeout (a float representing the number of seconds before an incomplete batch is published).
-
-# NOTE: BatchSeqLogger only works correctly with `with_logger` and added flush!
-# NOTE: Not yet parallelizable
 struct BatchSeqLogger <: AbstractLogger
     seqLogger::SeqLogger
     batchEvents::Vector{String}
     batchSize::Int
 end
 
-BatchSeqLogger(;serverUrl="http://localhost:5341",
+BatchSeqLogger(serverUrl="http://localhost:5341";
                minLevel=Logging.Info,
                apiKey="",
                batchSize=10,
                kwargs...) = begin
-   seqLogger = SeqLogger(;serverUrl=serverUrl,
+   seqLogger = SeqLogger(serverUrl;
                   minLevel=minLevel,
                   apiKey = apiKey,
                   kwargs...)
    return BatchSeqLogger(seqLogger, String[], batchSize)
 end
 
-function with_logger(@nospecialize(f::Function), batchSeqlogger::BatchSeqLogger)
-    with_logstate(f, LogState(batchSeqlogger))
+function Logging.with_logger(@nospecialize(f::Function), batchSeqlogger::BatchSeqLogger)
+    Base.CoreLogging.with_logstate(f, Base.CoreLogging.LogState(batchSeqlogger))
     flush(batchSeqlogger)
 end
 
@@ -45,22 +28,30 @@ Logging.shouldlog(logger::BatchSeqLogger, arg...) = true
 Logging.min_enabled_level(logger::BatchSeqLogger) = logger.seqLogger.minLevel
 Logging.catch_exceptions(logger::BatchSeqLogger) = false
 
+const l = ReentrantLock()
+
 function Logging.handle_message(logger::BatchSeqLogger, args...; kwargs...)
-    eventJson = parse_event_from_args(logger.seqLogger, args...; kwargs...)
-    push!(logger.batchEvents, eventJson)
-    if length(logger.batchEvents) >= logger.batchSize
-        flush(logger)
+    lock(l)
+    try
+        handleMessageArgs = LoggingExtras.handle_message_args(args...; kwargs...)
+        eventJson = parse_event_from_args(logger.seqLogger, handleMessageArgs)
+        push!(logger.batchEvents, eventJson)
+        if length(logger.batchEvents) >= logger.batchSize
+            flush(logger)
+        end
+    finally
+        unlock(l)
     end
     return nothing
 end
 
-function Base.flush(logger::BatchSeqLogger)
-    if !isempty(logger.batchEvents)
-        eventBatch = join(logger.batchEvents, "\n")
-        empty!(logger.batchEvents)
-        HTTP.request("POST", logger.seqLogger.serverUrl,
-                                            logger.seqLogger.header,
-                                            eventBatch)
+function Base.flush(batchLogger::BatchSeqLogger)
+    if !isempty(batchLogger.batchEvents)
+        eventBatch = join(batchLogger.batchEvents, "\n")
+        empty!(batchLogger.batchEvents)
+        HTTP.request("POST", batchLogger.seqLogger.serverUrl,
+                             batchLogger.seqLogger.header,
+                             eventBatch)
     end
     return nothing
 end
--- test/debug.jl
+++ test/debug.jl
@@ -11,8 +11,8 @@ using LinearAlgebra
 function parllel_iterations(logger, nIter, duration=1)
     Logging.with_logger(logger) do
         for i=1:nIter
-            @info "Iteration $i of {type}" type=typeof(logger)
-            Threads.@threads for i=1:4
+            Threads.@threads for idx=1:4
+                @info "Iteration $i,$idx of {type}" type=typeof(logger)
                 t = time()
                 x = randn(10,10)
                 while time() < t + duration
@@ -45,6 +45,21 @@ function idle_iterations(logger, nIter, duration=1)
     end
 end
 
+function parllel_iterations2(logger, nIter, duration=0.1)
+    Logging.with_logger(logger) do
+        Threads.@threads for idx=1:nIter
+            @info "Iteration $idx of {type}" type=typeof(logger)
+            t = time()
+            x = randn(10,10)
+            while time() < t + duration
+                x = x * (x.^2 + I)
+            end
+        end
+    end
+end
+
+# does not reaturn all log events
+@elapsed parllel_iterations2(batchSeqLogger, 10)
 
 ## Define Loggers
 serverUrl = "http://subdn215:5341/"
@@ -54,44 +69,47 @@ serialLogger = SeqLogger(serverUrl, SeqLoggers.Serial(); App="Trialrun")
 bgLogger = SeqLogger(serverUrl;App="Trialrun", Env="Test")
 bg3Logger = SeqLogger(serverUrl, SeqLoggers.Background(3); App="Trialrun", Env="Test")
 bgBrokenLogger = SeqLogger("brokenUrl", SeqLoggers.Parallel(); )
+batchSeqLogger = BatchSeqLogger(serverUrl; batchSize=10, App="Trialrun", Env="Test")
 
 ## Benchmarks
+# We only need, background, serial and batched logger (batched is the best (but parallelisation?))
 @elapsed busy_iterations(parallelLogger, 10)
 @elapsed busy_iterations(serialLogger, 10)
 @elapsed busy_iterations(bgLogger, 10)
 @elapsed busy_iterations(bg3Logger, 10)
+@elapsed busy_iterations(batchSeqLogger, 10)
 
 @elapsed busy_iterations(parallelLogger, 100, 0.1)
 @elapsed busy_iterations(serialLogger, 100, 0.1)
 @elapsed busy_iterations(bgLogger, 100, 0.1)
 @elapsed busy_iterations(bg3Logger, 100, 0.1)
+@elapsed busy_iterations(batchSeqLogger, 100, 0.1)
 
 @elapsed busy_iterations(parallelLogger, 25)
 @elapsed busy_iterations(serialLogger, 25)
 @elapsed busy_iterations(bgLogger, 25)
 @elapsed busy_iterations(bg3Logger, 25)
+@elapsed busy_iterations(batchSeqLogger, 25)
 
 @elapsed idle_iterations(parallelLogger, 10)
 @elapsed idle_iterations(serialLogger, 10)
 @elapsed idle_iterations(bgLogger, 10)
 @elapsed idle_iterations(bg3Logger, 10)
+@elapsed idle_iterations(batchSeqLogger, 10)
 
 @elapsed parllel_iterations(parallelLogger, 10)
 @elapsed parllel_iterations(serialLogger, 10)
 @elapsed parllel_iterations(bgLogger, 10)
 @elapsed parllel_iterations(bg3Logger, 10)
+@elapsed parllel_iterations(batchSeqLogger, 10)
 
 @elapsed parllel_iterations(bgBrokenLogger, 10)
 
 
 
 ##
-@time Logging.with_logger(seqLogger) do
-    @info "Info Event $(typeof(seqLogger)))"
-    sleep(0.1)
-end
 
-@time Logging.with_logger(seqLogger) do
+@time Logging.with_logger(batchSeqLogger) do
     @debug "Debug Event Welt"
     sleep(0.1)
     @info "Info Event"
@@ -103,14 +121,8 @@ end
 end
 
 # In combination with LoggingExtras.jl
-using SeqLoggers.LoggingExtras
-
-seqLogger = SeqLogger(; minLevel=Logging.Debug,
-                        apiKey="Test",
-                        App="Trialrun",
-                        HistoryId=raw"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-                        Env="Test")
-combinedLogger = TeeLogger(Logging.current_logger(), seqLogger)
+using LoggingExtras
+combinedLogger = TeeLogger(Logging.current_logger(), bgLogger)
 
 @time Logging.with_logger(combinedLogger) do
     @debug "Debug Event Welt"
@@ -121,77 +133,4 @@ combinedLogger = TeeLogger(Logging.current_logger(), seqLogger)
     sleep(0.1)
     @error "Error Event with multiple logger event properties: User: {User}, machine: {machine}" User="Ueli Wechsler" machine="speedy"
     sleep(0.1)
-end
-
-# Unnecessary!!!
-
-# using HTTP
-# event = "\"@t\":\"2020-10-17T19:36:30.266\",\"@mt\":\"Hallo Welt\",\"@l\":\"Information\",\"App\":\"DJSON\",\"Env\":\"Test\",\"HistoryId\":\"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx\","
-# res = HTTP.request("POST", seqLogger.serverUrl, seqLogger.header, "{$event}")
-
-
-batchSeqLogger = BatchSeqLogger(; batchSize=200, App="Trialrun", Env="Test")
-
-@time Logging.with_logger(batchSeqLogger) do
-    for i=1:1000
-        @info "Iteration $i of {type}" type=typeof(batchSeqLogger)
-        sleep(0.001)
-    end
-    flush(batchSeqLogger)
-end
-
-seqLogger = SeqLogger("http://subdn215:5341/", SeqLoggers.Serial(); App="Trialrun")
-seqLogger = SeqLogger("http://subdn215:5341/", SeqLoggers.Parallel(); App="Trialrun")
-seqLogger = SeqLogger("http://subdn215:5341/", SeqLoggers.ParallelDirty(); App="Trialrun")
-
-seqLogger = SeqLogger("http://subdn215:5341/";App="Trialrun", Env="Test")
-seqLogger = SeqLogger("http://subdn215:5341/", SeqLoggers.Background(3); App="Trialrun", Env="Test")
-
-
- # 15.514324 seconds (319.50 k allocations: 17.198 MiB)
-seqLogger = SeqLogger(SeqLoggers.Parallel(); App="Trialrun", Env="Test")
-seqLogger = SeqLogger(; App="Trialrun", Env="Test")
-@time Logging.with_logger(seqLogger) do
-    for i=1:250
-        @info "Iteration $i of {type}" type=typeof(seqLogger)
-        sleep(0.001)
-    end
-end
-
-using LinearAlgebra
-
-@time Logging.with_logger(seqLogger) do
-    for i=1:25
-        @info "Iteration $i of {type}" type=typeof(seqLogger)
-        t = time()
-        x = randn(10,10)
-        while time() < t + 1
-            x = x * (x.^2 + I)
-        end
-    end
-end
-
-@time Logging.with_logger(seqLogger) do
-    for i=1:25
-        @info "Iteration $i of {type}" type=typeof(seqLogger)
-        t = time()
-        x = randn(10,10)
-        while time() < t + 1
-            x = x * (x.^2 + I)
-        end
-    end
-end
-# 4.242601 seconds (538.67 k allocations: 30.332 MiB, 0.21% gc time)
-
-@time Logging.with_logger(seqLogger) do
-    for i=1:25
-        @info "Iteration $i of {type}" type=typeof(seqLogger)
-        Threads.@threads for i=1:4
-            t = time()
-            x = randn(10,10)
-            while time() < t + 1
-                x = x * (x.^2 + I)
-            end
-        end
-    end
 end
